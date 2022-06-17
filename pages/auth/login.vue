6fb7c00a91619af571f447f1e5099b24498d1a01
jpamulaklakin <jpamulaklakin@up.edu.ph>
2022-06-17 15:31:30 +0800

    Init

---
+++ pages/auth/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-btn @click="login">
+              Login with UPmail
+            </v-btn>
+</template>
+
+<script>
+export default {
+  layout: 'auth',
+  methods: {
+    login (message) {
+      window.location.href = `${process.env.API_BASE_URL}/auth/google`
+    }
+  }
+}
+</script>
