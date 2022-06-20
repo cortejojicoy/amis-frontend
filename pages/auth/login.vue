e4c0b9c050ba0d2f37b5d41bffcc9b66493e015f
unknown <raabrera@up.edu.ph>
2022-06-20 08:38:23 +0800

    initial design

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,10 +1,27 @@
 <template>
-    <button 
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        @click="login"
-    >
-              Login with UPmail
-    </button>
+
+            <div class="font-sans text-gray-900 antialiased">
+            <div class="h-screen grid justify-items-center items-center">
+        <div class="w-auto md:w-1/2 lg:w-1/3 py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+            <div class="flex justify-center -mt-16">
+                <img src="/logos/uplb-logo.png" class="object-center" />
+            </div>
+            <div class="mt-5">
+              <h2 class="text-gray-800 text-3xl font-semibold text-center">Welcome to the UPLB Academic Management Information System</h2>
+              <p class="mt-5 text-gray-600 text-center">This app is currently in alpha phase.</p>
+            </div>
+            <div class="flex justify-center mt-4">
+                <button 
+                    class="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
+                    @click="login"  
+                >
+                          Login with UPmail
+                </button>
+            </div>
+                        
+          </div>
+    </div>
+        </div>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <Tutorial/>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Good afternoon, {{ this.$auth.user.name }}!
+        </h2>
+        <p class="mt-3 text-gray-600">
+        
+Welcome to the new Academic Management Information System of UPLB! To start, we would like to inform you first that this system is still under development. So if you encounter any problems, errors, or bugs please send us an email to alert us about its existence. You may send your email to dx.uplb@up.edu.ph.
+
+To help you navigate easily through our system, the following are the modules that are currently working:
+
+Consent of Instructor Application
+This system is currently being co-created with UPLB Students and other stakeholders in coordination with the UPLB University Student Council, units and colleges.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-
   mounted () {
     history.replaceState(null, '', location.href.split('?')[0])
   }
