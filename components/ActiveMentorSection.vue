aa03ee2926135c30400c99a5bee5ef3dfa0534bc
jicoy <jicoy0120@LAPTOP-PD686PLI.localdomain>
2022-06-26 12:11:52 +0800

    Initial commit

---
+++ components/ActiveMentorSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="my-6">
+    <div class="flex justify-between mb-6">
+      <div class="text-2xl font-bold">Active Mentor Assignments</div>
+      <div class="">
+        <button class="p-2 bg-gray-300" disabled>
+          Generate Certificate of Mentor Assignment
+        </button>
+      </div>
+    </div>
+    <div class="bg-white overflow-auto shadow-xl sm:rounded-lg">
+      <table class="table-auto w-full items-center text-center">
+        <th>
+          <tr class="font-bold">
+            <td class="p-4">Mentor</td>
+            <td class="p-4">Roles</td>
+            <td class="p-4">Field Represented</td>
+          </tr>
+        </th>
+        <tbody>
+          <tr>
+            <td class="px-4 py-3"></td>
+            <td class="px-4 py-3"></td>
+            <td class="px-4 py-3"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ components/StudentInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mb-6">
+    <div class="text-2xl font-bold">
+      {{ this.$auth.user.name }}
+    </div>
+    <div class="mt-4 flex">
+      <div class="mr-36">
+        <div>
+          <span class="font-bold">Program:</span>
+        </div>
+        <div>
+          <span class="font-bold">Status:</span>
+        </div>
+      </div>
+      <div>
+        <div>
+          <span class="font-bold">UP Mail:</span>
+        </div>
+        <div>
+          <span class="font-bold">Mobile Number:</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
