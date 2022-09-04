82d6a0d04775a02957348e87a79236f49efab6b5
Marc Saulo <mcsaulo@up.edu.ph>
2022-09-04 21:22:08 +0800

    ui: update maintenance advisory in login page - ui: update advisory in faculty and student coi page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -13,7 +13,7 @@
           <p class="mt-5 text-gray-600 text-center">
             This app is currently in alpha phase.
           </p>
-          <p v-if="this.$config.APP_MAINTENANCE != ''" class="mt-4 text-xl text-orange-600 font-bold text-center">Maintenance Ongoing.</p>
+          <p v-if="this.$config.APP_MAINTENANCE != ''" class="mt-4 text-xl text-yellow-600 font-bold text-center">Maintenance ongoing until September 5, 2022 2PM.</p>
         </div>
         <div class="flex justify-center mt-4">
           <button class="bg-red-700 text-white py-2 px-4" @click="login">
