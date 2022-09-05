0a4410101cb12f6bc9eb9ed052c315d59b398bcd
Marc Saulo <mcsaulo@up.edu.ph>
2022-09-05 16:11:28 +0800

    ui: add notice of slow connection - ui: update maintenance advisory again

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -13,7 +13,8 @@
             <p class="mt-5 text-gray-600 text-center">
               This app is currently in alpha phase.
             </p>
-            <p v-if="this.$config.APP_MAINTENANCE != ''" class="mt-4 text-xl text-yellow-600 font-bold text-center">Our servers reached their limits during the pilot launch of AMIS Prerog. We are now reconfiguring and optimizing settings. Kindly check back at 5:00 PM. We will release another advisory by then. Thank you for your patience.</p>
+            <p v-if="this.$config.APP_MAINTENANCE != ''" class="mt-4 text-xl text-yellow-600 font-bold text-center">Maintenance Ongoing.</p>
+            <p class="mt-4 text-lg text-yellow-600 font-bold text-center">We are experiencing a high volume of users. You may experience some system instability or encounter errors.</p>
           </div>
           <div class="flex justify-center mt-4">
             <button class="bg-red-700 text-white py-2 px-4" @click="login">
